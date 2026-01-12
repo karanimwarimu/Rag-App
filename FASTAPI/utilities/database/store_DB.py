46974bb41abc3f8eeb049b196149c9bbd600c1d0
karanimwarimu <ianmwarimu42gmail.com>
2026-01-13 01:03:10 +0300

    supabase store embeddings successful

--- FASTAPI/utilities/database/store_DB.py
+++ FASTAPI/utilities/database/store_DB.py
@@ -1,6 +1,7 @@
 import logging
 from typing import List, Dict, Any
 from utilities.metadata import clean_metadata
+import json
 
 from .supabase_connect import db_connect
 
@@ -17,18 +18,23 @@ async def store_embedded_files(embedded_chunks: List[Dict[str, Any]]) -> str:
             chunk_id = chunk["metadata"]["chunk_uuid"]
             text = chunk["page_content"]
 
-            embedding = (
+            embeddin = (
                 chunk["embedding"].tolist()
                 if hasattr(chunk["embedding"], "tolist")
                 else chunk["embedding"]
             )
+            
+            embedding = f"[{','.join(map(str, embeddin))}]"
 
-            metadata = clean_metadata(chunk.get("metadata", {}))
+            #metadata = clean_metadata(chunk.get("metadata", {}))
+            
+            metadata = json.dumps(clean_metadata(chunk.get("metadata", {})))
 
             await conn.execute(
-                """
+            
+                    """
                 INSERT INTO documents (id, text, embedding, metadata)
-                VALUES ($1, $2, $3, $4)
+                VALUES ($1, $2, $3::vector, $4)
                 ON CONFLICT (id) DO NOTHING
                 """,
                 chunk_id,
